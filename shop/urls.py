826cf9e9b7f71f28485fc88a347c1a71c68daacf
sumayya <sbeevi89@gmail.com>
2023-10-05 07:47:10 -0700

    first

---
+++ shop/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path('', views.home, name='hm'),
+    path('<slug:c_slug>/', views.home, name='prod_cat'),
+    path('<slug:c_slug>/<slug:product_slug>', views.prodDetails, name='details'),
+    path('search', views.searching, name='search')
+]
